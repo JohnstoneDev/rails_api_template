7c3b3eb432dcf60d53f3d2735e918eb8ac00995a
JohnstoneDev <marucha.webdev@yahoo.com>
2022-12-01 00:51:29 +0300

    Add the deployment configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Hosts for deployment 
+  # config.hosts << 'your deployed app name should be here'
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
